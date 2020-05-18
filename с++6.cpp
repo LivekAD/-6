f4f0797d85742aec60e3400969506f59fbb93e26
Дмитрий Нечаев <62843800+LivekAD@users.noreply.github.com>
2020-05-18 16:09:24 +0300

    Add files via upload
    Виправлено

--- с++6.cpp
+++ с++6.cpp
@@ -4,23 +4,93 @@
 
 using namespace std;
 
-class Kos
+class armo
 {
 public:
-	int n = 3;
-	int* p = new int[n];
-	int size = 5;
-	int price = 1550;
+	virtual void kk() = 0;
+	virtual void mk() = 0;
+};
+
+class Kos : public armo
+{
+private:
+	int* ID2 = new int[1];
+	string cen = "15";
+	int col = 150;
+public:
 	string name = "Костюм";
+
+	Kos()
+	{
+		for (int i = 0; i < 1; i++)
+		{
+			ID2[i] = rand() % 10000;
+		}
+	}
+
+	string GetCena()
+	{
+		return cen;
+	}
+
+	void SetCena(string value)
+	{
+		cen = value;
+	}
+
+	virtual void mk()
+	{
+		int proch = 5, def = 15;
+		cout << proch << endl << def << endl;
+		int dem = (100 + 100 * (def / 100)) - rand() % 43;
+		cout << dem;
+	}
+
+	virtual void kk() override
+	{
+		cout << name << "\nID - " << ID2[0] << "\nCost - " << cen << "$" << endl;
+	}
 };
 
 class AKos : public Kos
 {
+private:
+	int* ID = new int[1];
+	string cena = "125";
+	int col2 = 30;
 public:
 	string edit = "Военный ";
-	int sizel = 10;
-	int* l = new int[n];
-	int price = 55000;
+
+	string GetCena()
+	{
+		return cena;
+	}
+
+	void SetCena(string value)
+	{
+		cena = value;
+	}
+
+	AKos()
+	{
+		for (int i = 0; i < 1; i++)
+		{
+			ID[i] = rand() % 878;
+		}
+	}
+
+	virtual void mk()
+	{
+		int proch = 25, def = 63;
+		cout << endl << proch << endl << def << endl;
+		int dem = (100 + 100 * (def / 100)) - rand() % 23;
+		cout << dem;
+	}
+
+	void kk() override
+	{
+		cout << name << " " << edit << "\nID - " << ID[0] << "\nCost - " << "$" << cena;
+	}
 };
 
 
@@ -28,25 +98,15 @@ public:
 int main()
 {
 	setlocale(LC_ALL, "Rus");
-	int i;
-	Kos j;
-	AKos jj;
-	for (i = 0; i < j.n; i++) 
-	{
-		cin >> j.p[i];
-		jj.l[i] = j.p[i] - 3;
-		if (jj.l[i] < 0)
-		{
-			jj.l[i] = 0;
-		}
-	}
-	cout << jj.name << " -> " << jj.edit << jj.name << endl;
-	for (size_t i = 0; i < j.n; i++)
-	{
-		cout << j.p[i] << " -> ";
-		jj.l[i] += j.p[i];
-		cout << jj.l[i] << endl;
-	}
-	cout << j.price << " -> " << jj.price << endl;
+
+	AKos k; Kos kk;
+	k.SetCena("15");
+	k.kk();
+	k.mk();
+	cout << endl << endl;
+	kk.SetCena("125");
+	kk.kk();
+	kk.mk();
+
 	return 0;
 }
